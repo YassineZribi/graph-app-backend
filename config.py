3054f67ff5b139eff1f2e90fee44c78ad481a1e4
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-12-26 04:34:05 +0100

    Use Environment Variables for Sensitive Data

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "my_secret_key"
-    JWT_SECRET_KEY = "my_jwt_secret_key"
-    MONGO_URI = "mongodb://localhost:27017/graph_db"
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    MONGO_URI = os.getenv('MONGO_URI')
